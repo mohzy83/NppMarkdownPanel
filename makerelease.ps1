2e4dc912b5e88ea083cd890287a846aea9236344
AQ <kubaszek@prz.edu.pl>
2020-06-25 10:15:33 +0200

    new makerelease.ps1, updt. style.css

--- makerelease.ps1
+++ makerelease.ps1
@@ -1,8 +1,19 @@
-function makeReleaseZip($filename, $targetPlattform)
-{
-	$zipName = "Release\NppMarkdownPanel-" + (Get-Item $filename).VersionInfo.FileVersion + "-" + $targetPlattform + ".zip"
-	Compress-Archive -LiteralPath $filename, 'README.md', 'License.txt', "NppMarkdownPanel\style.css" -DestinationPath $zipName -Force
-}
+# function makeReleaseZip($filename, $targetPlattform)
+# {
+# 	$zipName = "NppMarkdownPanel-" + (Get-Item $filename).VersionInfo.FileVersion + "-" + $targetPlattform + ".zip"
+# 	Compress-Archive -LiteralPath $filename, 'README.md', 'License.txt', "NppMarkdownPanel\style.css" , "NppMarkdownPanel\RegExp3.txt" -DestinationPath $zipName -Force
+# }
+# makeReleaseZip "NppMarkdownPanel\bin\Release\NppMarkdownPanel.dll" "x86"
+# makeReleaseZip "NppMarkdownPanel\bin\Release-x64\NppMarkdownPanel.dll" "x64"
 
-makeReleaseZip "NppMarkdownPanel\bin\Release\NppMarkdownPanel.dll" "x86"
-makeReleaseZip "NppMarkdownPanel\bin\Release-x64\NppMarkdownPanel.dll" "x64"
+$NppMdPanelDll = "NppMarkdownPanel\bin\Release-x64\NppMarkdownPanel.dll"
+$dest = "NppMarkdownPanel\bin\Release"
+
+Remove-Item "$dest*\*.pdb"
+foreach ($d in ("$dest-x64", "$dest")) {
+  foreach ($f in ("style.css", "RegExp3.txt", "..\README.md", "..\License.txt")) {
+    Copy-Item "NppMarkdownPanel\$f" -Destination $d -Force
+  }
+}
+($zipName = "NppMarkdownPanel-$((Get-Item $NppMdPanelDll).VersionInfo.FileVersion).zip")
+Compress-Archive -Path "$dest*" <#only paths below "...bin\"#> -DestinationPath $zipName -Force
