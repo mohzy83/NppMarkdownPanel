d60be8ec2fa6a858c18275e749a6c147a1f6ae40
Moritz Hasse <moritz@hasse-cloud.de>
2022-12-02 19:36:05 +0100

    #54 - update markdig to version 0.30.4 - load external markdig assembly

--- NppMarkdownPanel/Forms/MarkdownPreviewForm.cs
+++ NppMarkdownPanel/Forms/MarkdownPreviewForm.cs
@@ -3,7 +3,6 @@ using SHDocVw;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
 using System.Diagnostics;
 using System.Drawing;
 using System.IO;
---
+++ NppMarkdownPanel/MarkdigWrapperMarkdownGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace NppMarkdownPanel
+{
+    public class MarkdigWrapperMarkdownGenerator : IMarkdownGenerator
+    {
+        private Assembly assembly;
+        private Type type;
+        private Object wrapperInstance;
+
+        public MarkdigWrapperMarkdownGenerator()
+        {
+            var currentPluginPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var wrapperDllPath = Path.Combine(currentPluginPath, "lib", "MarkdigWrapper.dll");
+            assembly = Assembly.LoadFrom(wrapperDllPath);
+            type = assembly.GetType("MarkdigWrapper.Wrapper");
+            wrapperInstance  = Activator.CreateInstance(type);
+        }
+
+        public string ConvertToHtml(string markDownText, string filepath)
+        {
+            object[] methodParams = { markDownText, filepath };
+            object result = type.InvokeMember("ConvertToHtml",
+                  BindingFlags.Default | BindingFlags.InvokeMethod,
+                  null,
+                  wrapperInstance,
+                  methodParams);
+            return result.ToString();
+        }
+
+    }
+}
