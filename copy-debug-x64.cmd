3b3a2e7324ddb011ad9b1f5cd0cdcbbf027488a8
Moritz Hasse <moritz@hasse-cloud.de>
2022-12-04 17:35:22 +0100

    batch scripts to copy binaries

---
+++ copy-debug-x64.cmd
@@ -0,0 +1,2 @@
+xcopy 
+xcopy C:\development\NppMarkdownPanel\NppMarkdownPanel\bin\Debug-x64\*.* C:\npp\npp64\plugins\NppMarkdownPanel /c /s /r /d /y /i
---
+++ copy-debug-x86.cmd
@@ -0,0 +1,2 @@
+xcopy 
+xcopy C:\development\NppMarkdownPanel\NppMarkdownPanel\bin\Debug\*.* C:\npp\npp32\plugins\NppMarkdownPanel /c /s /r /d /y /i
