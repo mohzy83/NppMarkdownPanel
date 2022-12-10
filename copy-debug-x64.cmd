f00da19a1adec648b06fd2e5d937ad27bdba436e
Moritz Hasse <moritz@hasse-cloud.de>
2022-12-10 21:56:54 +0100

    fix copy cmds

--- copy-debug-x64.cmd
+++ copy-debug-x64.cmd
@@ -1,2 +1,1 @@
-xcopy 
 xcopy C:\development\NppMarkdownPanel\NppMarkdownPanel\bin\Debug-x64\*.* C:\npp\npp64\plugins\NppMarkdownPanel /c /s /r /d /y /i
--- copy-debug-x86.cmd
+++ copy-debug-x86.cmd
@@ -1,2 +1,1 @@
-xcopy 
 xcopy C:\development\NppMarkdownPanel\NppMarkdownPanel\bin\Debug\*.* C:\npp\npp32\plugins\NppMarkdownPanel /c /s /r /d /y /i
