0efed19946b1ff0c46f042f22ffe6aee56163d85
Moritz Hasse <moritz@hasse-cloud.de>
2023-01-14 11:51:46 +0100

    #74 postprocessor template added

---
+++ PPExtensions/MdpPostprocessorTemplate/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MdpPostprocessorTemplate
+{
+    class Program
+    {
+        // Commandline Arguments
+        // args[0] : Input file path
+        // args[1] : Output file path
+        static void Main(string[] args)
+        {
+            if (args.Length >= 2)
+            {
+                var inputFile = args[0];
+                var ouputFile = args[1];
+                if (File.Exists(inputFile))
+                {
+                    string fileContent = File.ReadAllText(inputFile);
+                    // process file content
+                    string output = fileContent.ToUpper();
+                    File.WriteAllText(ouputFile, output);
+                }
+                else
+                {
+                    Console.WriteLine("Input file :" + inputFile + " is not present.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Missing Paramters");
+            }
+        }
+    }
+}
