26919166fadc338d22e3a328fc19e0706a3ad938
Moritz Hasse <moritz@hasse-cloud.de>
2023-01-14 14:05:26 +0100

    template name changed

--- PPExtensions/MdpPrePostprocessorTemplate/Program.cs
+++ PPExtensions/MdpPrePostprocessorTemplate/Program.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MdpPostprocessorTemplate
+namespace MdpPrePostprocessorTemplate
 {
     class Program
     {
--- PPExtensions/MdpPrePostprocessorTemplate/Properties/AssemblyInfo.cs
+++ PPExtensions/MdpPrePostprocessorTemplate/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // Allgemeine Informationen über eine Assembly werden über die folgenden
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("MdpPostprocessorTemplate")]
+[assembly: AssemblyTitle("MdpPrePostprocessorTemplate")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("HP Inc.")]
-[assembly: AssemblyProduct("MdpPostprocessorTemplate")]
-[assembly: AssemblyCopyright("Copyright © HP Inc. 2023")]
+[assembly: AssemblyProduct("MdpPrePostprocessorTemplate")]
+[assembly: AssemblyCopyright("Copyright © 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
